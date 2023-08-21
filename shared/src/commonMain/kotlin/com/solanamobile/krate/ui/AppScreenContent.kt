e8554109a320140479c6aeaba413edd476384754
creativedrewy <Andrew Watson andrew.watson@solanamobile.com>
2023-08-21 15:13:56 -0700

    Refactor a theme file

--- shared/src/commonMain/kotlin/com/solanamobile/krate/ui/AppScreenContent.kt
+++ shared/src/commonMain/kotlin/com/solanamobile/krate/ui/AppScreenContent.kt
@@ -1,22 +1,13 @@
 package com.solanamobile.krate.ui
 
 import androidx.compose.animation.ExperimentalAnimationApi
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Typography
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.font.FontStyle
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.unit.sp
 import cafe.adriel.voyager.core.registry.ScreenRegistry
 import cafe.adriel.voyager.navigator.Navigator
 import cafe.adriel.voyager.transitions.SlideTransition
 import com.solanamobile.krate.createscreen.CreateScreen
 import com.solanamobile.krate.extension.NavScreenProvider
 import com.solanamobile.krate.profilescreen.ProfileScreen
-import com.solanamobile.krate.resources.font
 import com.solanamobile.krate.startscreen.StartScreen
 
 @OptIn(ExperimentalAnimationApi::class)
@@ -34,55 +25,7 @@ fun AppScreenContent() {
         }
     }
 
-    val geographNormal = FontFamily(
-        font(
-            name = "GeographBlack",
-            res = "testgeograph_black",
-            weight = FontWeight.Black,
-            style = FontStyle.Normal
-        ),
-        font(
-            name = "GeographMedium",
-            res = "testgeograph_medium",
-            weight = FontWeight.Medium,
-            style = FontStyle.Normal
-        )
-    )
-
-    val typography = Typography(
-        defaultFontFamily = geographNormal,
-        h2 = TextStyle(
-            fontFamily = geographNormal,
-            fontWeight = FontWeight.Black,
-            fontSize = 54.sp,
-            lineHeight = 60.sp
-        ),
-        h3 = TextStyle(
-            fontFamily = geographNormal,
-            fontWeight = FontWeight.Black,
-            fontSize = 32.sp,
-            lineHeight = 60.sp
-        ),
-        h4 = TextStyle(
-            fontFamily = geographNormal,
-            fontWeight = FontWeight.Medium,
-            fontSize = 32.sp
-        ),
-        h5 = TextStyle(
-            fontFamily = geographNormal,
-            fontWeight = FontWeight.Medium,
-            fontSize = 16.sp
-        )
-    )
-
-    MaterialTheme(
-        colors = MaterialTheme.colors.copy(
-            background = Color(0xFFF0E7E0),
-            surface = Color(0xFFF0DCD4),
-            primary = Color(0xFFF07C6C),
-        ),
-        typography = typography
-    ) {
+    KrateAppTheme {
         Navigator(
             screen = ScreenRegistry.get(NavScreenProvider.StartScreen)
         ) {
---
+++ shared/src/commonMain/kotlin/com/solanamobile/krate/ui/AppTheme.kt
@@ -0,0 +1,69 @@
+package com.solanamobile.krate.ui
+
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Typography
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+import com.solanamobile.krate.resources.font
+
+@Composable
+fun KrateAppTheme(
+    content: @Composable () -> Unit
+) {
+    val geographNormal = FontFamily(
+        font(
+            name = "GeographBlack",
+            res = "testgeograph_black",
+            weight = FontWeight.Black,
+            style = FontStyle.Normal
+        ),
+        font(
+            name = "GeographMedium",
+            res = "testgeograph_medium",
+            weight = FontWeight.Medium,
+            style = FontStyle.Normal
+        )
+    )
+
+    val typography = Typography(
+        defaultFontFamily = geographNormal,
+        h2 = TextStyle(
+            fontFamily = geographNormal,
+            fontWeight = FontWeight.Black,
+            fontSize = 54.sp,
+            lineHeight = 60.sp
+        ),
+        h3 = TextStyle(
+            fontFamily = geographNormal,
+            fontWeight = FontWeight.Black,
+            fontSize = 32.sp,
+            lineHeight = 60.sp
+        ),
+        h4 = TextStyle(
+            fontFamily = geographNormal,
+            fontWeight = FontWeight.Medium,
+            fontSize = 32.sp
+        ),
+        h5 = TextStyle(
+            fontFamily = geographNormal,
+            fontWeight = FontWeight.Medium,
+            fontSize = 16.sp
+        )
+    )
+
+    MaterialTheme(
+        colors = MaterialTheme.colors.copy(
+            background = Color(0xFFF0E7E0),
+            surface = Color(0xFFF0DCD4),
+            primary = Color(0xFFF07C6C),
+        ),
+        typography = typography
+    ) {
+        content()
+    }
+}
