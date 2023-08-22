1c47af1d99604cfaf0e09b469437c13bf1feaf3b
creativedrewy <Andrew Watson andrew.watson@solanamobile.com>
2023-08-22 15:44:38 -0700

    More formatting

--- profileScreen/src/commonMain/kotlin/com/solanamobile/krate/profilescreen/ProfileScreen.kt
+++ profileScreen/src/commonMain/kotlin/com/solanamobile/krate/profilescreen/ProfileScreen.kt
@@ -7,20 +7,17 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.wrapContentWidth
-import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.ScrollableTabRow
 import androidx.compose.material.Tab
 import androidx.compose.material.TabRowDefaults
-import androidx.compose.material.TabRowDefaults.tabIndicatorOffset
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import cafe.adriel.voyager.core.screen.Screen
 import com.solanamobile.krate.extension.getScreenModel
@@ -59,32 +56,24 @@ fun ProfileScreenContent(
                 .background(MaterialTheme.colors.surface)
         )
 
-        val pagerState = rememberPagerState()
-
         ScrollableTabRow(
-//            modifier = Modifier
-//                .fillMaxWidth(),
+            modifier = Modifier
+                .padding(
+                    top = 80.dp,
+                    start = 24.dp,
+                    end = 24.dp
+                ),
             backgroundColor = MaterialTheme.colors.background,
             selectedTabIndex = 0,
-            edgePadding = 0.dp,
-            indicator = { tabPositions ->
-                TabRowDefaults.Indicator(
-                    modifier = Modifier
-                        .height(8.dp)
-                        .tabIndicatorOffset(tabPositions[0])
+            edgePadding = (-16).dp,
+            divider = {
+                TabRowDefaults.Divider(
+                    thickness = 4.dp,
+                    color = MaterialTheme.colors.onSurface
                 )
-            },
-//            divider = {
-//                TabRowDefaults.Divider(
-//                    thickness = 4.dp,
-//                    color = MaterialTheme.colors.onSurface
-//                )
-//            }
+            }
         ) {
             Tab(
-                modifier = Modifier
-                    .wrapContentWidth()
-                    .background(Color.Green),
                 text = {
                     Text(
                         text = "Creations",
