f4c893d8da7d10d5b171e6805ea735295f384ba3
creativedrewy <Andrew Watson andrew.watson@solanamobile.com>
2023-08-23 11:59:53 -0700

    Get rid of unnecessary stuff

--- createScreen/src/commonMain/kotlin/com/solanamobile/krate/createscreen/viewmodel/CreateScreenViewModel.kt
+++ createScreen/src/commonMain/kotlin/com/solanamobile/krate/createscreen/viewmodel/CreateScreenViewModel.kt
@@ -1,19 +1,14 @@
 package com.solanamobile.krate.createscreen.viewmodel
 
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.ImageBitmap
 import cafe.adriel.voyager.core.model.StateScreenModel
 import cafe.adriel.voyager.core.model.coroutineScope
 import com.moriatsushi.koject.Provides
 import com.solanamobile.krate.createscreen.repository.GetImgRepository
-import com.solanamobile.krate.extension.graphics.toImageBitmap
 import kotlinx.coroutines.delay
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import org.jetbrains.compose.resources.ExperimentalResourceApi
-import org.jetbrains.compose.resources.resource
 import kotlin.io.encoding.ExperimentalEncodingApi
 
 sealed class ViewState() {
@@ -27,39 +22,11 @@ sealed class ViewState() {
     ): ViewState()
 }
 
-@Composable
-fun <T, U> Map<T, U>.isReady(block: @Composable () -> Unit) {
-    if (this.isNotEmpty()) {
-        block()
-    }
-}
-
 @OptIn(ExperimentalResourceApi::class)
 @Provides
 class CreateScreenViewModel(
     private val getImgRepository: GetImgRepository,
 ): StateScreenModel<ViewState>(ViewState.Prompting) {
-
-    private val _resources: MutableStateFlow<Map<String, ImageBitmap>> = MutableStateFlow(mapOf())
-
-    val resources = _resources.asStateFlow()
-
-    fun loadResources() {
-        coroutineScope.launch {
-            _resources.update {
-                mapOf(
-                    "star" to resource("loading_star.png").readBytes().toImageBitmap(),
-                    "star" to resource("loading_star.png").readBytes().toImageBitmap(),
-                    "circle" to resource("loading_circle.png").readBytes().toImageBitmap(),
-                    "triangle" to resource("loading_triangle.png").readBytes().toImageBitmap(),
-                    "wallet1" to resource("wallet.png").readBytes().toImageBitmap(),
-                    "wallet2" to resource("wallet2.png").readBytes().toImageBitmap(),
-                    "wallet3" to resource("wallet3.png").readBytes().toImageBitmap(),
-//                    "wallet4" to resource("wallet4.png").readBytes().toImageBitmap()
-                )
-            }
-        }
-    }
 
     @OptIn(ExperimentalEncodingApi::class)
     fun generateImageFromPrompt(prompt: String) {
