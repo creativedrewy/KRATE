e7daf646bbdca7f05168006a5e1aa0f1b75871fe
creativedrewy <Andrew Watson andrew.watson@solanamobile.com>
2023-09-06 14:55:06 -0700

    Get screens more compatible with proper layout

--- createScreen/src/commonMain/kotlin/com/solanamobile/krate/createscreen/CreateScreen.kt
+++ createScreen/src/commonMain/kotlin/com/solanamobile/krate/createscreen/CreateScreen.kt
@@ -25,9 +25,13 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.asPaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.ime
+import androidx.compose.foundation.layout.navigationBars
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
@@ -389,22 +393,18 @@ fun CreateScreenContent(
                 ) { targetState ->
                     when (targetState) {
                         ViewState.Prompting -> {
-                            Column(
+                            Box(
                                 modifier = Modifier
-                                    .fillMaxSize(),
-                                horizontalAlignment = Alignment.CenterHorizontally
+                                    .fillMaxSize()
+                                    .padding(
+                                        bottom = WindowInsets.navigationBars.asPaddingValues().calculateBottomPadding()
+                                    ),
+                                contentAlignment = Alignment.BottomCenter
                             ) {
-                                Spacer(
-                                    modifier = Modifier
-                                        .fillMaxWidth()
-                                        .weight(1f)
-                                )
-
-                                //TODO: Make this dock above the keyboard when typing
                                 Button(
                                     modifier = Modifier
                                         .padding(
-                                            bottom = 30.dp
+                                            bottom = 8.dp + WindowInsets.ime.asPaddingValues().calculateBottomPadding()
                                         )
                                         .size(84.dp),
                                     shape = CircleShape,
