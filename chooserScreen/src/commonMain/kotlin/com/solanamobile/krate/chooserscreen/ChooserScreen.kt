a24c483e88409c64a9c57b1f39738f85911704af
creativedrewy <Andrew Watson andrew.watson@solanamobile.com>
2023-09-19 14:10:38 -0700

    Finish up initial chooser screen layout

--- chooserScreen/src/commonMain/kotlin/com/solanamobile/krate/chooserscreen/ChooserScreen.kt
+++ chooserScreen/src/commonMain/kotlin/com/solanamobile/krate/chooserscreen/ChooserScreen.kt
@@ -1,7 +1,9 @@
 package com.solanamobile.krate.chooserscreen
 
 import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
@@ -22,11 +24,14 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.material.TopAppBar
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowForward
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import cafe.adriel.voyager.core.registry.ScreenRegistry
 import cafe.adriel.voyager.core.screen.Screen
@@ -119,11 +124,59 @@ fun ChooserScreenContent() {
                         top = 8.dp
                     )
                     .width(320.dp)
-                    .height(205.dp),
+                    .height(205.dp)
+                    .clickable {
+                        navigator.push(ScreenRegistry.get(NavScreenProvider.CameraScreen))
+                    },
                 shape = RoundedCornerShape(12.dp),
-                border = BorderStroke(2.dp, MaterialTheme.colors.onSurface)
+                border = BorderStroke(2.dp, MaterialTheme.colors.onSurface),
+                elevation = 8.dp
             ) {
+                Column {
+                    Row(
+                        modifier = Modifier
+                            .padding(
+                                top = 40.dp
+                            )
+                            .fillMaxWidth(),
+                        horizontalArrangement = Arrangement.Center
+                    ) {
+                        ResourceImage(
+                            modifier = Modifier
+                                .width(83.dp)
+                                .height(72.dp),
+                            resourceName = "teleport_left.png"
+                        )
 
+                        Image(
+                            modifier = Modifier
+                                .padding(
+                                    top = 16.dp
+                                )
+                                .size(24.dp),
+                            imageVector = Icons.Default.ArrowForward,
+                            contentDescription = null
+                        )
+
+                        ResourceImage(
+                            modifier = Modifier
+                                .width(77.dp)
+                                .height(65.dp),
+                            resourceName = "teleport_right.png"
+                        )
+                    }
+
+                    Text(
+                        modifier = Modifier
+                            .padding(
+                                top = 24.dp
+                            )
+                            .fillMaxWidth(),
+                        text = "Take a selfie and teleport yourself somewhere completely new.",
+                        style = MaterialTheme.typography.h6,
+                        textAlign = TextAlign.Center
+                    )
+                }
             }
 
             Text(
@@ -134,6 +187,72 @@ fun ChooserScreenContent() {
                 text = "Text to Image",
                 style = MaterialTheme.typography.h6
             )
+
+            Card(
+                modifier = Modifier
+                    .padding(
+                        top = 8.dp
+                    )
+                    .width(320.dp)
+                    .height(205.dp)
+                    .clickable {
+                        navigator.push(ScreenRegistry.get(NavScreenProvider.CreateScreen(
+                            createMode = NavScreenProvider.CreateMode.Text2Img
+                        )))
+                    },
+                shape = RoundedCornerShape(12.dp),
+                border = BorderStroke(2.dp, MaterialTheme.colors.onSurface),
+                elevation = 8.dp
+            ) {
+                Column {
+                    Row(
+                        modifier = Modifier
+                            .padding(
+                                top = 20.dp
+                            )
+                            .fillMaxWidth(),
+                        horizontalArrangement = Arrangement.Center,
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        ResourceImage(
+                            modifier = Modifier
+                                .width(78.dp)
+                                .height(91.dp),
+                            resourceName = "text_left.png"
+                        )
+
+                        Image(
+                            modifier = Modifier
+                                .padding(
+                                    top = 16.dp
+                                )
+                                .size(24.dp),
+                            imageVector = Icons.Default.ArrowForward,
+                            contentDescription = null
+                        )
+
+                        ResourceImage(
+                            modifier = Modifier
+                                .width(77.dp)
+                                .height(65.dp),
+                            resourceName = "text_right.png"
+                        )
+                    }
+
+                    Text(
+                        modifier = Modifier
+                            .padding(
+                                top = 24.dp,
+                                start = 8.dp,
+                                end = 8.dp
+                            )
+                            .fillMaxWidth(),
+                        text = "Type anything you like. We'll make an image for you.",
+                        style = MaterialTheme.typography.h6,
+                        textAlign = TextAlign.Center
+                    )
+                }
+            }
         }
     }
 }
