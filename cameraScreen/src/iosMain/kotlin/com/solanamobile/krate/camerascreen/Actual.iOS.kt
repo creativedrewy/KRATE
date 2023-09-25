c06ecd4979fb956dde85dcad033654f46ce40596
creativedrewy <Andrew Watson andrew.watson@solanamobile.com>
2023-09-25 14:51:08 -0700

    Simplify code and layout for iOS camera

--- cameraScreen/src/iosMain/kotlin/com/solanamobile/krate/camerascreen/Actual.iOS.kt
+++ cameraScreen/src/iosMain/kotlin/com/solanamobile/krate/camerascreen/Actual.iOS.kt
@@ -1,36 +1,22 @@
 package com.solanamobile.krate.camerascreen
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.BoxScope
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material.CircularProgressIndicator
-import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.icons.materialIcon
-import androidx.compose.material.icons.materialPath
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ImageBitmap
-import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.interop.UIKitView
-import androidx.compose.ui.unit.dp
 import kotlinx.cinterop.CValue
 import kotlinx.cinterop.ExperimentalForeignApi
-import kotlinx.cinterop.ObjCAction
 import platform.AVFoundation.AVAuthorizationStatusAuthorized
 import platform.AVFoundation.AVAuthorizationStatusDenied
 import platform.AVFoundation.AVAuthorizationStatusNotDetermined
@@ -47,33 +33,18 @@ import platform.AVFoundation.AVCaptureDeviceTypeBuiltInWideAngleCamera
 import platform.AVFoundation.AVCapturePhoto
 import platform.AVFoundation.AVCapturePhotoCaptureDelegateProtocol
 import platform.AVFoundation.AVCapturePhotoOutput
-import platform.AVFoundation.AVCapturePhotoSettings
 import platform.AVFoundation.AVCaptureSession
 import platform.AVFoundation.AVCaptureSessionPresetPhoto
-import platform.AVFoundation.AVCaptureVideoOrientationLandscapeLeft
-import platform.AVFoundation.AVCaptureVideoOrientationLandscapeRight
-import platform.AVFoundation.AVCaptureVideoOrientationPortrait
 import platform.AVFoundation.AVCaptureVideoPreviewLayer
 import platform.AVFoundation.AVLayerVideoGravityResizeAspectFill
 import platform.AVFoundation.AVMediaTypeVideo
-import platform.AVFoundation.AVVideoCodecKey
-import platform.AVFoundation.AVVideoCodecTypeJPEG
 import platform.AVFoundation.authorizationStatusForMediaType
 import platform.AVFoundation.fileDataRepresentation
-import platform.AVFoundation.position
 import platform.AVFoundation.requestAccessForMediaType
 import platform.CoreGraphics.CGRect
-import platform.CoreLocation.CLLocationManager
-import platform.CoreLocation.kCLLocationAccuracyBest
 import platform.Foundation.NSError
-import platform.Foundation.NSNotification
-import platform.Foundation.NSNotificationCenter
-import platform.Foundation.NSSelectorFromString
 import platform.QuartzCore.CATransaction
 import platform.QuartzCore.kCATransactionDisableActions
-import platform.UIKit.UIDevice
-import platform.UIKit.UIDeviceOrientation
-import platform.UIKit.UIDeviceOrientationDidChangeNotification
 import platform.UIKit.UIImage
 import platform.UIKit.UIView
 import platform.darwin.NSObject
@@ -163,20 +134,20 @@ private fun BoxScope.RealDeviceCamera(
     camera: AVCaptureDevice,
 ) {
     val capturePhotoOutput = remember { AVCapturePhotoOutput() }
-    var actualOrientation by remember {
-        mutableStateOf(
-            AVCaptureVideoOrientationPortrait
-        )
-    }
-    val locationManager = remember {
-        CLLocationManager().apply {
-            desiredAccuracy = kCLLocationAccuracyBest
-            requestWhenInUseAuthorization()
-        }
+    var capturePhotoStarted by remember { mutableStateOf(false) }
+
+    val captureSession: AVCaptureSession = remember {
+        val captureSession = AVCaptureSession()
+        captureSession.sessionPreset = AVCaptureSessionPresetPhoto
+
+        val captureDeviceInput = AVCaptureDeviceInput.deviceInputWithDevice(device = camera, error = null)!!
+        captureSession.addInput(captureDeviceInput)
+        captureSession.addOutput(capturePhotoOutput)
+
+        captureSession
     }
 
-//    val nameAndDescription = createNewPhotoNameAndDescription()
-    var capturePhotoStarted by remember { mutableStateOf(false) }
+    val cameraPreviewLayer = remember { AVCaptureVideoPreviewLayer(session = captureSession) }
 
     val photoCaptureDelegate = remember {
         object : NSObject(), AVCapturePhotoCaptureDelegateProtocol {
@@ -189,73 +160,10 @@ private fun BoxScope.RealDeviceCamera(
                 if (photoData != null) {
 
                     val uiImage = UIImage(photoData)
-//                    onCapture(
-//                        createCameraPictureData(
-//                            name = nameAndDescription.name,
-//                            description = nameAndDescription.description,
-//                            gps = gps
-//                        ),
-//                        IosStorableImage(uiImage)
-//                    )
                 }
 
                 capturePhotoStarted = false
             }
-        }
-    }
-
-    val captureSession: AVCaptureSession = remember {
-        AVCaptureSession().also { captureSession ->
-            captureSession.sessionPreset = AVCaptureSessionPresetPhoto
-            val captureDeviceInput: AVCaptureDeviceInput =
-                AVCaptureDeviceInput.deviceInputWithDevice(device = camera, error = null)!!
-            captureSession.addInput(captureDeviceInput)
-            captureSession.addOutput(capturePhotoOutput)
-        }
-    }
-    val cameraPreviewLayer = remember {
-        AVCaptureVideoPreviewLayer(session = captureSession)
-    }
-
-    DisposableEffect(Unit) {
-        class OrientationListener : NSObject() {
-            @Suppress("UNUSED_PARAMETER")
-            @ObjCAction
-            fun orientationDidChange(arg: NSNotification) {
-                val cameraConnection = cameraPreviewLayer.connection
-                if (cameraConnection != null) {
-                    actualOrientation = when (UIDevice.currentDevice.orientation) {
-                        UIDeviceOrientation.UIDeviceOrientationPortrait -> AVCaptureVideoOrientationPortrait
-                        UIDeviceOrientation.UIDeviceOrientationLandscapeLeft -> AVCaptureVideoOrientationLandscapeRight
-                        UIDeviceOrientation.UIDeviceOrientationLandscapeRight -> AVCaptureVideoOrientationLandscapeLeft
-                        UIDeviceOrientation.UIDeviceOrientationPortraitUpsideDown -> AVCaptureVideoOrientationPortrait
-                        else -> cameraConnection.videoOrientation
-                    }
-
-                    cameraConnection.videoOrientation = actualOrientation
-                }
-
-                capturePhotoOutput.connectionWithMediaType(AVMediaTypeVideo)?.videoOrientation = actualOrientation
-            }
-        }
-
-        val listener = OrientationListener()
-        val notificationName = UIDeviceOrientationDidChangeNotification
-
-        NSNotificationCenter.defaultCenter.addObserver(
-            observer = listener,
-            selector = NSSelectorFromString(
-                OrientationListener::orientationDidChange.name + ":"
-            ),
-            name = notificationName,
-            `object` = null
-        )
-        onDispose {
-            NSNotificationCenter.defaultCenter.removeObserver(
-                observer = listener,
-                name = notificationName,
-                `object` = null
-            )
         }
     }
 
@@ -264,6 +172,7 @@ private fun BoxScope.RealDeviceCamera(
         background = Color.Black,
         factory = {
             val cameraContainer = UIView()
+
             cameraContainer.layer.addSublayer(cameraPreviewLayer)
             cameraPreviewLayer.videoGravity = AVLayerVideoGravityResizeAspectFill
             captureSession.startRunning()
@@ -272,109 +181,25 @@ private fun BoxScope.RealDeviceCamera(
         onResize = { view: UIView, rect: CValue<CGRect> ->
             CATransaction.begin()
             CATransaction.setValue(true, kCATransactionDisableActions)
+
             view.layer.setFrame(rect)
             cameraPreviewLayer.setFrame(rect)
             CATransaction.commit()
         },
     )
 
-    CircularButton(
-        imageVector = IconPhotoCamera,
-        modifier = Modifier.align(Alignment.BottomCenter).padding(36.dp),
-        enabled = !capturePhotoStarted,
-    ) {
-        capturePhotoStarted = true
-        val photoSettings = AVCapturePhotoSettings.photoSettingsWithFormat(
-            format = mapOf(AVVideoCodecKey to AVVideoCodecTypeJPEG)
-        )
-
-        if (camera.position == AVCaptureDevicePositionFront) {
-            capturePhotoOutput.connectionWithMediaType(AVMediaTypeVideo)?.automaticallyAdjustsVideoMirroring = false
-            capturePhotoOutput.connectionWithMediaType(AVMediaTypeVideo)?.videoMirrored = true
-        }
-
-        capturePhotoOutput.capturePhotoWithSettings(
-            settings = photoSettings,
-            delegate = photoCaptureDelegate
-        )
-    }
-
-    if (capturePhotoStarted) {
-        CircularProgressIndicator(
-            modifier = Modifier.size(80.dp).align(Alignment.Center),
-            color = Color.White.copy(alpha = 0.7f),
-            strokeWidth = 8.dp,
-        )
-    }
-}
-
-@Composable
-fun CircularButton(
-    imageVector: ImageVector,
-    modifier: Modifier = Modifier,
-    enabled: Boolean = true,
-    onClick: () -> Unit,
-) {
-    CircularButton(
-        modifier = modifier,
-        content = {
-            Icon(imageVector, null, Modifier.size(34.dp), Color.White)
-        },
-        enabled = enabled,
-        onClick = onClick
-    )
-}
-
-@Composable
-fun CircularButton(
-    content: @Composable () -> Unit,
-    modifier: Modifier = Modifier,
-    enabled: Boolean,
-    onClick: () -> Unit,
-) {
-    Box(
-        modifier
-            .size(60.dp)
-            .clip(CircleShape)
-            .run {
-                if (enabled) {
-                    clickable { onClick() }
-                } else this
-            },
-        contentAlignment = Alignment.Center,
-    ) {
-        content()
-    }
-}
-
-// TODO Copied from "material:material-icons-extended", because this artifact is not working on iOS for now
-val IconPhotoCamera = materialIcon(name = "Filled.PhotoCamera") {
-    materialPath {
-        moveTo(12.0f, 12.0f)
-        moveToRelative(-3.2f, 0.0f)
-        arcToRelative(3.2f, 3.2f, 0.0f, true, true, 6.4f, 0.0f)
-        arcToRelative(3.2f, 3.2f, 0.0f, true, true, -6.4f, 0.0f)
-    }
-    materialPath {
-        moveTo(9.0f, 2.0f)
-        lineTo(7.17f, 4.0f)
-        lineTo(4.0f, 4.0f)
-        curveToRelative(-1.1f, 0.0f, -2.0f, 0.9f, -2.0f, 2.0f)
-        verticalLineToRelative(12.0f)
-        curveToRelative(0.0f, 1.1f, 0.9f, 2.0f, 2.0f, 2.0f)
-        horizontalLineToRelative(16.0f)
-        curveToRelative(1.1f, 0.0f, 2.0f, -0.9f, 2.0f, -2.0f)
-        lineTo(22.0f, 6.0f)
-        curveToRelative(0.0f, -1.1f, -0.9f, -2.0f, -2.0f, -2.0f)
-        horizontalLineToRelative(-3.17f)
-        lineTo(15.0f, 2.0f)
-        lineTo(9.0f, 2.0f)
-        close()
-        moveTo(12.0f, 17.0f)
-        curveToRelative(-2.76f, 0.0f, -5.0f, -2.24f, -5.0f, -5.0f)
-        reflectiveCurveToRelative(2.24f, -5.0f, 5.0f, -5.0f)
-        reflectiveCurveToRelative(5.0f, 2.24f, 5.0f, 5.0f)
-        reflectiveCurveToRelative(-2.24f, 5.0f, -5.0f, 5.0f)
-        close()
-    }
+//capturePhotoStarted = true
+//val photoSettings = AVCapturePhotoSettings.photoSettingsWithFormat(
+//    format = mapOf(AVVideoCodecKey to AVVideoCodecTypeJPEG)
+//)
+//
+//if (camera.position == AVCaptureDevicePositionFront) {
+//    capturePhotoOutput.connectionWithMediaType(AVMediaTypeVideo)?.automaticallyAdjustsVideoMirroring = false
+//    capturePhotoOutput.connectionWithMediaType(AVMediaTypeVideo)?.videoMirrored = true
+//}
+//
+//capturePhotoOutput.capturePhotoWithSettings(
+//    settings = photoSettings,
+//    delegate = photoCaptureDelegate
+//)
 }
