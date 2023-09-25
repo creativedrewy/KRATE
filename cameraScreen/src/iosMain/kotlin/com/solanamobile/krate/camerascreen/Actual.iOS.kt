54f449856aef0e4cfb7cda1e0f736252c2e727a1
creativedrewy <Andrew Watson andrew.watson@solanamobile.com>
2023-09-25 12:39:56 -0700

    Get Camera permission authorization working

--- cameraScreen/src/iosMain/kotlin/com/solanamobile/krate/camerascreen/Actual.iOS.kt
+++ cameraScreen/src/iosMain/kotlin/com/solanamobile/krate/camerascreen/Actual.iOS.kt
@@ -5,8 +5,27 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.ImageBitmap
+import platform.AVFoundation.AVAuthorizationStatusAuthorized
+import platform.AVFoundation.AVAuthorizationStatusDenied
+import platform.AVFoundation.AVAuthorizationStatusNotDetermined
+import platform.AVFoundation.AVAuthorizationStatusRestricted
+import platform.AVFoundation.AVCaptureDevice
+import platform.AVFoundation.AVMediaTypeVideo
+import platform.AVFoundation.authorizationStatusForMediaType
+import platform.AVFoundation.requestAccessForMediaType
+
+private sealed interface CameraAccess {
+    object Undefined : CameraAccess
+    object Denied : CameraAccess
+    object Authorized : CameraAccess
+}
 
 actual class PermissionState(
     granted: Boolean = false,
@@ -19,7 +38,34 @@ actual class PermissionState(
 
 @Composable
 actual fun getPermissionState(): PermissionState {
-    return PermissionState()
+    var cameraAccess: CameraAccess by remember { mutableStateOf(CameraAccess.Undefined) }
+
+    LaunchedEffect(Unit) {
+        when (AVCaptureDevice.authorizationStatusForMediaType(AVMediaTypeVideo)) {
+            AVAuthorizationStatusAuthorized -> {
+                cameraAccess = CameraAccess.Authorized
+            }
+
+            AVAuthorizationStatusDenied, AVAuthorizationStatusRestricted -> {
+                cameraAccess = CameraAccess.Denied
+            }
+
+            AVAuthorizationStatusNotDetermined -> {
+                AVCaptureDevice.requestAccessForMediaType(
+                    mediaType = AVMediaTypeVideo
+                ) { success ->
+                    cameraAccess = if (success) CameraAccess.Authorized else CameraAccess.Denied
+                }
+            }
+        }
+    }
+
+    return PermissionState(
+        granted = cameraAccess is CameraAccess.Authorized,
+        action = {
+            AVCaptureDevice.authorizationStatusForMediaType(AVMediaTypeVideo)
+        }
+    )
 }
 
 @Composable
